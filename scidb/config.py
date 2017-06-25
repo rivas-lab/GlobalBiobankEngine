a073e7eca9ae2b0321c205eca71a59454f3b4cd6
Rares Vernica <rvernica@gmail.com>
2017-06-24 20:31:49 -0700

    ICD loader in Python similar to R implementation * use FIFO pipes (reuse FIFO files) * chunk input files per number of instances * load icd_index (bulk created) * load icd

--- scidb/config.py
+++ scidb/config.py
@@ -1,10 +1,76 @@
 import os.path
 
+
+SCIDB_INSTANCE_NUM = 8
 GBE_DATA_PATH = '/home/scidb/GlobalBiobankEngine/gbe_data'
 
-QC_FILTER_PATH = os.path.join(GBE_DATA_PATH, 'qc')
-QC_FILTER_FILES = (
-    {'filename': os.path.join(QC_FILTER_PATH, 'UKBioBiLallfreqSNPexclude.dat'),
+
+# -- -
+# -- - QC - --
+# -- -
+QC_PATH = os.path.join(GBE_DATA_PATH, 'qc')
+QC_FILES = (
+    {'filename': os.path.join(QC_PATH, 'UKBioBiLallfreqSNPexclude.dat'),
      'header': 1},
-    {'filename': os.path.join(QC_FILTER_PATH, 'ukb_ukbl_low_concordance.dat')}
+    {'filename': os.path.join(QC_PATH, 'ukb_ukbl_low_concordance.dat')}
 )
+
+QC_ARRAY = 'qc'
+
+
+# -- -
+# -- - ICD - --
+# -- -
+ICD_GLOB = os.path.join(GBE_DATA_PATH, 'icdassoc', 'hybrid', 'c*.hybrid.gz')
+
+ICD_INDEX_ARRAY = 'icd_index'
+ICD_INDEX_SCHEMA = "<icd:string>[icd_id]"
+
+ICD_ARRAY = 'icd'
+ICD_SCHEMA = """
+  <affyid:      string,
+   or_val:      double,
+   se:          double,
+   pvalue:      double,
+   lor:         double,
+   log10pvalue: double,
+   l95or:       double,
+   u95or:       double>
+  [icd_id    = 0:*,20,0,
+   chrom     = 1:25,1,0,
+   pos       = 0:*,10000000,0,
+   synthetic = 0:999,1000,0]"""
+ICD_QUERY = """
+  insert(
+    redimension(
+      apply(
+        filter(
+          index_lookup(
+            aio_input(
+              'paths={paths}',
+              'instances={instances}',
+              'num_attributes=12') as INPUT,
+            {qc},
+            INPUT.a2,
+            is_in_filter),
+          substr(a0, 0, 1) <> '#' and
+          a6 = 'ADD' and
+          is_in_filter is null and
+          dcast(a9, double(null)) < .5 and
+          a11 <> 'NA' and
+          dcast(a11, double(null)) <> 0),
+        icd_id,      {icd_id_cond},
+        chrom,       int64(a0),
+        pos,         int64(a1),
+        affyid,      a2,
+        or_val,      dcast(a8,  double(null)),
+        se,          dcast(a9,  double(null)),
+        pvalue,      dcast(a11, double(null)),
+        lor,         log(dcast(a8, double(null))),
+        log10pvalue, log10(dcast(a11, double(null))) * -1,
+        l95or,       exp(log(dcast(a8, double(null))) -
+                         1.96 * dcast(a9, double(null))),
+        u95or,       exp(log(dcast(a8, double(null))) +
+                         1.96 * dcast(a9, double(null)))),
+      {icd}),
+    {icd})"""
--- scidb/loader.py
+++ scidb/loader.py
@@ -1,7 +1,12 @@
 import config
+import glob
+import itertools
 import logging
 import numpy
+import os
 import scidbpy
+import subprocess
+import tempfile
 
 
 logging.basicConfig(level=logging.INFO)
@@ -10,21 +15,109 @@ logger = logging.getLogger(__name__)
 db = scidbpy.connect()
 
 
-def load_qc_filters():
-    qc_filters = []
-    for rec in config.QC_FILTER_FILES:
+def load_qc():
+    qc = []
+    for rec in config.QC_FILES:
         with open(rec['filename']) as fp:
             lines = fp.readlines()
             skip = rec.get('header', 0)
-            qc_filters.extend(
+            qc.extend(
                 l.strip().split()[0] for l in lines[:-skip if skip else None])
             logger.info('QC:file:%s lines:%d skip:%d',
                         rec['filename'], len(lines), skip)
 
     # TODO fix when upload_data=list() implemented
-    db.input(upload_data=numpy.array(qc_filters)).store('qc_filter')
-    logger.info('Array:qc_filter records:%d', len(qc_filters))
+    db.input(upload_data=numpy.array(qc)).store(config.QC_ARRAY)
+    logger.info('Array:%s records:%d', config.QC_ARRAY, len(qc))
+
+
+def load_icd():
+    icd_lst = list(set(get_icd(fn) for fn in glob.iglob(config.ICD_GLOB)))
+    icd_id_map = dict(zip(icd_lst, range(len(icd_lst))))
+    db.input(config.ICD_INDEX_SCHEMA,
+             upload_data=numpy.array(icd_lst)).store(config.ICD_INDEX_ARRAY)
+
+    db.create_array(config.ICD_ARRAY, config.ICD_SCHEMA)
+    try:
+        fifo_names = [make_fifo() for i in range(config.SCIDB_INSTANCE_NUM)]
+        file_iter = [glob.iglob(config.ICD_GLOB)] * config.SCIDB_INSTANCE_NUM
+        instances = [str(i) for i in range(config.SCIDB_INSTANCE_NUM)]
+
+        for file_names in itertools.zip_longest(*file_iter):
+
+            # Last chunk might have None, strip them out
+            if None in file_names:
+                file_names = [file_name
+                              for file_name in file_names if file_name]
+
+            # Map filenames to icd_id based on src_instance_id
+            icd_id_cond = 'null'
+            for (inst, file_name) in zip(range(len(file_names)), file_names):
+                icd_id = icd_id_map[get_icd(file_name)]
+                icd_id_cond = ('iif(src_instance_id = {inst}, ' +
+                               '{icd_id}, {prev})').format(
+                                   inst=inst, icd_id=icd_id, prev=icd_id_cond)
+
+            # Make pipes
+            logger.info('Pipes:starting %d...', len(file_names))
+            pipes = [make_pipe(file_name, fifo_name)
+                     for (file_name, fifo_name) in zip(file_names, fifo_names)]
+
+            query = config.ICD_QUERY.format(
+                icd=config.ICD_ARRAY,
+                qc=config.QC_ARRAY,
+                paths=';'.join(fifo_names[:len(file_names)]),
+                instances=';'.join(instances[:len(file_names)]),
+                icd_id_cond=icd_id_cond)
+
+            logger.info('Query:starting...')
+            db.iquery(query)
+            logger.info('Query:done')
+
+            logger.info('Pipes:return code:%s',
+                        ','.join(str(pipe.poll()) for pipe in pipes))
+    finally:
+        for fifo_name in fifo_names:
+            remove_fifo(fifo_name)
+
+
+def get_icd(filename):
+    return os.path.basename(filename).split('.')[1]
+
+
+def make_pipe(file_name, fifo_name):
+    cmd = 'zcat {} > {}'.format(file_name, fifo_name)
+    proc = subprocess.Popen(cmd, shell=True)
+
+    logger.info('Spawn:%s pid:%d', cmd, proc.pid)
+    return proc
+
+
+def make_fifo():
+    fifo_dir = tempfile.mkdtemp()
+    fifo_name = os.path.join(fifo_dir, 'fifo')
+    os.mkfifo(fifo_name)
+
+    logger.info('FIFO:%s', fifo_name)
+    return fifo_name
+
+
+def remove_arrays():
+    for array in (config.QC_ARRAY,
+                  config.ICD_INDEX_ARRAY,
+                  config.ICD_ARRAY):
+        try:
+            db.remove(array)
+        except:
+            pass
+
+
+def remove_fifo(fifo_name):
+    os.remove(fifo_name)
+    os.rmdir(os.path.dirname(fifo_name))
 
 
 if __name__ == '__main__':
-    load_qc_filters()
+    remove_arrays()
+    load_qc()
+    load_icd()
