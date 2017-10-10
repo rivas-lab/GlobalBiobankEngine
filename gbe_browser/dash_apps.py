e65e3a4d1d67b2cfe0db636c9ab2223a2ac18053
cdeboever3 <cdeboever3@gmail.com>
2017-10-10 22:08:22 +0000

    Updated genetic correlation app
    Axis fix, hierarchical clustering.

--- gbe_browser/dash_apps.py
+++ gbe_browser/dash_apps.py
@@ -7,6 +7,9 @@ import os
 
 import numpy as np
 import pandas as pd
+pd.options.mode.chained_assignment = None  # default='warn'
+import scipy.spatial.distance as dist
+import scipy.cluster.hierarchy as sch
 
 def initialize():
     fn = os.path.join('static/gcorr/opt_corr.tsv')
@@ -31,22 +34,45 @@ def initialize():
     starting_phenos = list(vc.head(40).index)
     return(data, phenos, starting_phenos)
 
-def make_plot_df(df):
-    ind = sorted(list(set(df['p1']) | set(df['p2'])))
-    tdf = df[df.apply(lambda x: x['p1'] in ind and x['p2'] in ind, axis=1)]
-    sindex = pd.Series(range(len(ind)), index=ind)
+def make_plot_df(selected_phenos):
+    tdf = DATA[DATA.p1.isin(selected_phenos) & DATA.p2.isin(selected_phenos)]
+    if tdf.shape[0] > 2:
+        tdf_m = tdf.pivot_table(values='omegacor21', index='p1', columns='p2').fillna(0)
+        if tdf_m.shape[0] < len(selected_phenos):
+            missing = list(set(selected_phenos) - set(tdf_m.index))
+            tdf_m = pd.concat([tdf_m, pd.DataFrame(0, index=missing,
+                                                   columns=missing)]).fillna(0)
+        distmat = dist.pdist(tdf_m, metric='euclidean')
+        # Allow the following methods 'single', 'complete', 'average', 'weighted'
+        # and 'ward' because they are O(n^2)
+        hclust = sch.linkage(distmat, method='complete')
+        dend = sch.dendrogram(hclust, no_plot=True)
+        order = pd.Series(tdf_m.index, index=range(tdf_m.shape[0]))[dend['leaves']]
+    else:
+        order = selected_phenos
+    # Include all phenotypes in sindex regardless of whether they have any
+    # estimates in tdf.
+    sindex = pd.Series(1 + np.arange(len(selected_phenos)), index=order)
     tdf['xind'] = sindex[tdf.p1].values
     tdf['yind'] = sindex[tdf.p2].values
-    # tdf['size'] = -np.log10(tdf.drawp)
-    tdf['size'] = np.log(tdf.drawz)
-    tdf['size'] = tdf['size'] - tdf['size'].min()
-    tdf['size'] = tdf['size'] / tdf['size'].max()
-    tdf['size'] = tdf['size'] * 100 + 50
+    minz = np.log(3)
+    maxz = np.log(4.5)
+    minsize = 50.
+    maxsize = 130
+    m = (maxsize - minsize) / (maxz - minz)
+    b = maxsize - m * maxz
+    tdf.loc[:, 'size'] = np.log(tdf.drawz) * m + b
+    tdf.loc[tdf['size'] > maxsize, 'size'] = maxsize
     return(tdf, sindex)
 
 def gcorr_scatter(selected_phenos):
-    tdf = DATA[DATA.p1.isin(selected_phenos) & DATA.p2.isin(selected_phenos)]
-    tdf,sindex = make_plot_df(tdf)
+    if len(selected_phenos) > 0:
+        tdf,sindex = make_plot_df(selected_phenos)
+    else:
+        # If there are no selected phenos, we'll just show the starting data
+        # until the user makes a selection.
+        tdf,sindex = make_plot_df(STARTING_PHENOS)
+
     text = ('gcorr: ' + tdf.omegacor21.round(3).astype(str) + '<BR>' + 
             'gcorr SE: ' + tdf.omegacor21_se.round(3).astype(str) + '<BR>' + 
             'z: ' + tdf.drawz.round(3).astype(str) + '<BR>' + 
@@ -54,7 +80,7 @@ def gcorr_scatter(selected_phenos):
             'tau1: ' + tdf.tau1.round(3).astype(str) +  '<BR>' + 
             'tau2: ' + tdf.tau2.round(3).astype(str))
     layout = go.Layout(
-        height=1000,
+        height=800,
         width=1200,
         hovermode='closest',
         xaxis=dict(
@@ -62,18 +88,19 @@ def gcorr_scatter(selected_phenos):
             tickvals=sindex,
             showticklabels=True,
             fixedrange=True,
+            range=[0, sindex.shape[0] + 1.5],
         ),
         yaxis=dict(
             ticktext=sindex.index,
             tickvals=sindex,
             showticklabels=True,
             fixedrange=True,
+            range=[0, sindex.shape[0] + 1.5],
         ),
         margin=dict(
-            l=300,
-            r=80,
-            b=300,
-            t=10,
+            l=250,
+            r=250,
+            b=150,
         )
     )
     data = [go.Scatter(
@@ -86,6 +113,8 @@ def gcorr_scatter(selected_phenos):
             color = tdf['omegacor21'],
             colorbar=dict(title='Genetic correlation'),
             colorscale='RdBu',
+            cmin=-1,
+            cmax=1,
             showscale=True
         )
     )]
